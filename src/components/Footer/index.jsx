ac2de19d6f86241915cb6c4768f56da4a4d1f297
Charlie Carter <charliec1665@gmail.com>
2023-10-12 23:07:06 -0400

    completed footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import NavBar from 'react-bootstrap/NavBar'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import logo from '../../assets/icons/icon5.png'
 
 const Footer = () => {
   return (
     <>
         <footer>
-            <NavBar id='footer' class='justify-around'>
-                    {/* <Row className=''> */}
-                        <Col className=''>
-                            <img 
-                                src={logo}
-                                width={40}
-                                height={40}
-                                className='m-3'
-                                alt='Wine love icon'
-                            />
-                        </Col>
-                        <Col class=''>
-                            <p class=''>Andy Charlie Thinh Quincy</p>
-                        </Col>
-                    {/* </Row> */}
+            <NavBar id='footer' className='flex justify-center'>
+                <a href='https://github.com/abilodeau21'>Andy</a>
+                <a href='https://github.com/charliec1665'>Charlie</a>
+                <img 
+                    src={logo}
+                    width={40}
+                    height={40}
+                    className='m-3'
+                    alt='Wine love icon'
+                />
+                <a href='https://github.com/hnihT212'>Thinh</a>
+                <a href='https://github.com/qclaytor30'>Quincy</a>
             </NavBar>
         </footer>
     </>
